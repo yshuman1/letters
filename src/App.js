9a7c129f280dd87807186c47276e619c20143902
Yasin Shuman <email@yasin.io>
2017-12-30 00:00:50 -0800

    added slider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import logo from "./logo.svg";
 import "./App.css";
+import Slider from "./components/Slider/Slider";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
+        <Slider />
       </div>
     );
   }
