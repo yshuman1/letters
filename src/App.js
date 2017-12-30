fbaeb8b47f375e82b40a7b23b16ca0e43d547608
Yasin Shuman <email@yasin.io>
2017-12-30 15:07:16 -0800

    removed carousel and added jumbotron

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import NavBar from "./components/NavBar/NavBar";
 import logo from "./logo.svg";
 import "./App.css";
 import Slider from "./components/Slider/Slider";
+import MainPic from "./components/MainPic/MainPic";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
-        <Slider />
+        <MainPic />
       </div>
     );
   }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -36,35 +36,35 @@ export default class NavBar extends React.Component {
       <container>
         <Row>
           <Col>
-            <div>
-              <Navbar color="faded" light expand="md">
-                <NavbarBrand href="/">Letters to Palestine</NavbarBrand>
-                <NavbarToggler onClick={this.toggle} />
-                <Collapse isOpen={this.state.isOpen} navbar>
-                  <Nav className="ml-auto" navbar>
-                    <NavItem>
-                      <NavLink href="/components/">Search</NavLink>
-                    </NavItem>
-                    <NavItem>
-                      <NavLink href="https://github.com/reactstrap/reactstrap">
-                        FAQ
-                      </NavLink>
-                    </NavItem>
-                    <UncontrolledDropdown nav inNavbar>
-                      <DropdownToggle nav caret>
-                        Options
-                      </DropdownToggle>
-                      <DropdownMenu>
-                        <DropdownItem>Option 1</DropdownItem>
-                        <DropdownItem>Option 2</DropdownItem>
-                        <DropdownItem divider />
-                        <DropdownItem>Reset</DropdownItem>
-                      </DropdownMenu>
-                    </UncontrolledDropdown>
-                  </Nav>
-                </Collapse>
-              </Navbar>
-            </div>
+            <Navbar color="dark" light expand="md">
+              <NavbarBrand className="logo-color" href="/">
+                Letters to Palestine
+              </NavbarBrand>
+              <NavbarToggler onClick={this.toggle} />
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink href="/components/">Search</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="https://github.com/reactstrap/reactstrap">
+                      FAQ
+                    </NavLink>
+                  </NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Options
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem>Option 1</DropdownItem>
+                      <DropdownItem>Option 2</DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem>Reset</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </Nav>
+              </Collapse>
+            </Navbar>
           </Col>
         </Row>
       </container>
