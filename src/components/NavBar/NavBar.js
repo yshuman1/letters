ee6cfaf0d75e64fb1b6bcf61dd558a762d4399ad
Yasin Shuman <email@yasin.io>
2017-12-29 23:53:59 -0800

    first commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,73 @@
+import React from "react";
+import "./NavBar.css";
+import {
+  Container,
+  Row,
+  Col,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <container>
+        <Row>
+          <Col>
+            <div>
+              <Navbar color="faded" light expand="md">
+                <NavbarBrand href="/">Letters to Palestine</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                  <Nav className="ml-auto" navbar>
+                    <NavItem>
+                      <NavLink href="/components/">Search</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink href="https://github.com/reactstrap/reactstrap">
+                        FAQ
+                      </NavLink>
+                    </NavItem>
+                    <UncontrolledDropdown nav inNavbar>
+                      <DropdownToggle nav caret>
+                        Options
+                      </DropdownToggle>
+                      <DropdownMenu>
+                        <DropdownItem>Option 1</DropdownItem>
+                        <DropdownItem>Option 2</DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem>Reset</DropdownItem>
+                      </DropdownMenu>
+                    </UncontrolledDropdown>
+                  </Nav>
+                </Collapse>
+              </Navbar>
+            </div>
+          </Col>
+        </Row>
+      </container>
+    );
+  }
+}
